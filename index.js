d1aa52432688d51161be31e8c4ebd48aba07f9d8
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-03-14 19:30:24 +0200

    done-1

---
+++ index.js
@@ -0,0 +1,47 @@
+import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.table(list);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const added = await addContact(name, email, phone);
+      console.log(added);
+      break;
+
+    case "remove":
+      const removed = await removeContact(id);
+      console.log(removed);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
